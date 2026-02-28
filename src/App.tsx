9a61149b318156cd29106fe7d3c68b9202ff51d2
Msadha27 <sadha272007@gmail.com>
2026-02-27 21:50:25 -0800

    Fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+// Capitalized component imports
 import Fashion from "./pages/Fashion";
 import Books from "./pages/Books";
 import Finds from "./pages/Finds";
-
 
 const queryClient = new QueryClient();
 
@@ -19,13 +20,14 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-           <Route path="/" element={<Index />} />
-           <Route path="/fashion" element={<Fashion />} />
-           <Route path="/books" element={<Books />} />
-           <Route path="/finds" element={<Finds />} />
-           <Route path="*" element={<NotFound />} />
-        </Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/fashion" element={<Fashion />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/finds" element={<Finds />} />
 
+          {/* Catch-all route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
